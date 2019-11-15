d539b185962c3e0de179eaca4a9c2e43021d6744
Thomas Hallberg <thomashallberg@Thomass-Air.attlocal.net>
2019-11-15 11:26:17 -0500

    added import for welcome and character pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+      <Route path='/' component={Header} />
+      <Route exact path='/' component={WelcomePage} />
+      <Route path='/Character' component={CharacterList} />
     </main>
   );
 }
