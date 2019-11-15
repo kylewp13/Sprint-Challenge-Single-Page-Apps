bd4e5a824d6f96c24ba8c8c5838fca4e909edfea
Thomas Hallberg <thomashallberg@Thomass-Air.attlocal.net>
2019-11-15 12:35:23 -0500

    reconfigured imports and routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
-import WelcomePage from './components/WelcomePage';
-import CharacterList from './components/CharacterList';
-
+import  WelcomePage from "./components/WelcomePage"
+import { Route } from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
-      <Route path='/' component={Header} />
-      <Route exact path='/' component={WelcomePage} />
-      <Route path='/Character' component={CharacterList} />
+      <Route exact path="/" component={WelcomePage} />
+      <Route  path="/characters" component={Header} />
     </main>
   );
 }
